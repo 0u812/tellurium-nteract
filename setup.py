815cf199459404d39f2ebdab6e81ac9f8d3fb9d1
0u812 <0u812@users.noreply.github.com>
2017-04-12 21:51:30 -0700

    add install deps

--- setup.py
+++ setup.py
@@ -7,4 +7,25 @@ setup(name             = 'tellurium-nteract',
       author           = 'J Kyle Medley',
       url              ='https://github.com/0u812/tellurium-nteract' ,
       packages         = [],
+      install_requires = [
+      # tellurium
+      'tellurium>=2.0.0',
+      # core packages
+      'libroadrunner>=1.4.16', 'rrplugins>=1.1.8',
+      'tesbml>=5.15.0', 'tesedml>=0.4.2', 'tecombine>=0.2.0',
+      'antimony>=2.9.1', 'phrasedml>=1.0.5', 'sbml2matlab>=0.9.1',
+      # cust
+      'temagics>=1.0.0',
+      # jupyter
+      'jupyter-core>=4.3.0', 'jupyter-client>=4.4.0',
+      'ipython>=5.2.2', 'ipykernel>=4.5.2',
+      # aux
+      'plotly>=2.0.5',
+      'Jinja2>=2.9.5',
+      'appdirs>=1.4.3',
+      # should be inherited
+      'numpy>=1.12.0', 'matplotlib>=2.0.0',
+      # misc
+      'lmfit>=0.9.5',
+      ],
 )
