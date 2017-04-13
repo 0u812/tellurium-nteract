0693b71304bad3d92a50157f62498aa75e614baf
0u812 <0u812@users.noreply.github.com>
2017-04-12 21:41:37 -0700

    initl commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name             = 'tellurium-nteract',
+      version          = "2.0.0",
+      description      = 'Metapackage for installing Tellurium nteract deployment',
+      license          = 'Apache 2.0',
+      author           = 'J Kyle Medley',
+      url              ='https://github.com/0u812/tellurium-nteract' ,
+      packages         = [],
+)
